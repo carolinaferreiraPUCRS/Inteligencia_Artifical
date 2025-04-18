32c8356d24650806481ef2a7693a2f750f532083
GustavoWMSilva <willian.gustavo@edu.pucrs.br>
2025-04-17 22:53:13 -0300

    Finalização da Criação do dataset e como foi implementado.

--- Trabalho01/balanceandoDados.py
+++ Trabalho01/balanceandoDados.py
@@ -1,7 +1,7 @@
 from imblearn.under_sampling import NearMiss
 import pandas as pd
 
-data = pd.read_csv('newdata.csv', header=None)
+data = pd.read_csv('./data/para-criar-dados.data', header=None)
 X = data.iloc[:, :9]
 Y = data.iloc[:, 9]
 
@@ -10,4 +10,4 @@ X_balanceado, Y_balanceado = nm.fit_resample(X, Y)
 
 # Junta e salva
 df_resultado = pd.concat([X_balanceado, Y_balanceado], axis=1)
-df_resultado.to_csv('dados_balanceados.csv', index=False)
+df_resultado.to_csv('hasGame_balanceado.csv', index=False)
